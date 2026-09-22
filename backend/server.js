fc7ebca9aa10ead8185db1deb2c71a906a9dc42c
Ubuntu <ubuntu@ip-172-31-6-188.ap-south-1.compute.internal>
2026-09-22 11:10:36 +0000

    Update app name on staging

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
-const APP_NAME = process.env.APP_NAME || "Docker Deployment App";
+const APP_NAME = process.env.APP_NAME || "Docker Deployment App - STAGING";
 
 app.get("/", (req, res) => {
   res.send(`
